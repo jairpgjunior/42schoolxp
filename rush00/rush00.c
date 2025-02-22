3aa1f5d8a3067b90e6961013c4a678eabb6773b0
Jair Junior <11341232+jairpgjunior@users.noreply.github.com>
2025-02-22 10:26:34 +0000

    Update rush00.c

--- rush00/rush00.c
+++ rush00/rush00.c
@@ -1,11 +1,22 @@
 void ft_putchar(char c);
 
-void draw_char()
+void draw_char(int row, int col, int x, int y)
 {
+  char  corner_tl = 'o';
+  char  corner_tr = 'o';
+  char  corner_bl = 'o';
+  char  corner_br = 'o';
 
+  // char  hor = '-';
+  // char  ver = '|';
+  
 }
 
-void rush()
+void rush(int x, int y)
 {
+  int  row;
+  int  col;
 
+  if ( x <= 0 || y <= 0)
+    return;
 }
